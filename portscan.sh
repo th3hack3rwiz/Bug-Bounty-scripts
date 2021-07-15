17f0b1c13a3b9f756dada3960a984b04f28fa0fd
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-07-16 05:01:34 +0530

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -3,7 +3,7 @@ user=$(whoami)
 cat ${1}.ipList.txt | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > buff ; cat buff > ${1}.ipList.txt ; rm buff
 echo -e "[+] Starting masscan..."
 sudo masscan -iL ${1}.ipList.txt -p0-65535 --rate=10000 -oL scan.txt
-sudo chown $user: $user scan.txt
+sudo chown $user:$user scan.txt
 cat scan.txt | awk '{print $3,$4}' | sed '/^[[:space:]]*$/d' >> port_ip 
 rm scan.txt
 for i in $(cat port_ip | awk '{print $2}' | grep -o -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" |sort -u); do ports=$(cat port_ip | grep $i | awk '{print $1}' | sort -u | xargs |>
