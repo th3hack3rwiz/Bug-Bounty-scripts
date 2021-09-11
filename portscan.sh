7d0f9fc01481a5f25789e935034cd2dea71c49ae
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-09-11 17:16:17 +0530

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -6,12 +6,13 @@ sudo masscan -iL ${1}.ipList.txt -p0-65535 --rate=10000 -oL scan.txt
 sudo chown $user:$user scan.txt
 cat scan.txt | awk '{print $3,$4}' | sed '/^[[:space:]]*$/d' >> port_ip 
 rm scan.txt
-for i in $(cat port_ip | awk '{print $2}' | grep -o -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" |sort -u); do ports=$(cat port_ip | grep $i | awk '{print $1}' | sort -u | xargs |>
+for i in $(cat port_ip | awk '{print $2}' | grep -o -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" |sort -u); do ports=$(cat port_ip | grep $i | awk '{print $1}' | sort -u | xargs | sed 's/[[:space:]]/,/g') ; echo "$i $ports" | anew -q nmapme; done
 file=nmapme
 while read i
  do
   port=$(echo $i | awk '{print $2}')
   ip=$(echo $i | awk '{print $1}')
   #echo $port $ip
-  sudo nmap -sV -Pn -A -p $port $ip -sC -T4 --script vulners.nse | tee -a nmap_results
+  sudo nmap -sV -Pn -A -p $port $ip -sC -T4 --script vulners.nse,vulscan/vulscan.nse | tee -a nmap_results
 done < $file 
+rm nmapme
